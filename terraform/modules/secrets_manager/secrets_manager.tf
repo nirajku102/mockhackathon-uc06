d302ca751fe066231210f291e151a7a77b6767e3
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-21 23:33:15 +0530

    Update secrets_manager.tf

--- terraform/modules/secrets_manager/secrets_manager.tf
+++ terraform/modules/secrets_manager/secrets_manager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_password" {
-  name = "aurora/mysql/password"
+  name = "mockhackathon/mysql/password"
 }
 
 resource "aws_secretsmanager_secret_version" "db_password_version" {
