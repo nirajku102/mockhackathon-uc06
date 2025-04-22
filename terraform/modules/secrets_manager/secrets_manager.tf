5accdd6c6351b3d751ea4eea062fe735c8e38329
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-22 05:05:13 +0000

    [create-pull-request] automated change

--- terraform/modules/secrets_manager/secrets_manager.tf
+++ terraform/modules/secrets_manager/secrets_manager.tf
@@ -3,6 +3,6 @@ resource "aws_secretsmanager_secret" "db_password" {
 }
 
 resource "aws_secretsmanager_secret_version" "db_password_version" {
-  secret_id = aws_secretsmanager_secret.db_password.id
+  secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = var.db_password
 }
